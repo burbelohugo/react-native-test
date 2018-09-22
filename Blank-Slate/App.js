66597961874f8244b9417253f2f3013b71ce33a3
Hugo Burbelo <hugoburbelo@gmail.com>
2018-09-22 19:54:07 -0400

    Experiment with Justify-content

--- Blank-Slate/App.js
+++ Blank-Slate/App.js
@@ -32,10 +32,10 @@ class Blink extends Component {
 export default class App extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 10, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 10, backgroundColor: 'steelblue'}} />
+      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
+        <View style={{flex: .10, backgroundColor: 'powderblue'}} />
+        <View style={{flex: .2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: .10, backgroundColor: 'steelblue'}} />
       </View>
     )
   }
